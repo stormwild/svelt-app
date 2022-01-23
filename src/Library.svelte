c7040f0fc6b2de048258b0836d0fab7bc1cb0943
Alexander R. Torrijos <artorrijos@gmail.com>
2022-01-23 20:14:39 +0800

    Initial commit

---
+++ src/Library.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<style>
+  header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+</style>
+
+  
+<header>
+  <span class="preamble">Welcome to the</span>
+  <h1>Library</h1>
+</header>
+
+<p class="greeting">
+  This is a library for the people. Welcome. Read the books here. Be inspired.
+  Go home, and share them with your family.
+</p>
